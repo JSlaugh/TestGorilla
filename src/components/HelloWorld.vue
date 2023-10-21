5836c001e71fa1688fe169bde4275dca35fbd341
JSlaugh <J@slaugh.net>
2023-10-20 21:20:58 -0600

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div>
+      <h2>Options</h2>
+      <div
+        v-for="(stateOption, key) in StateOptions"
+        :key="key"
+        @click="filterLoanOfficer(stateOption)"
+        style="display: inline; padding-right: 1rem; text-decoration: underline"
+      >
+        {{ stateOption }}
+      </div>
+    </div>
+    <div
+      v-for="(loanOfficer, key) in loanOfficers.filter(
+        (item) => item.state == filterValue
+      )"
+      :key="key"
+    >
+      <div>
+        <img :src="loanOfficer.imageUrl" :alt="loanOfficer.name" />
+        <p>
+          {{ loanOfficer.sid }}: {{ loanOfficer.name }}: {{ loanOfficer.city }},
+          {{ loanOfficer.state }}: {{ loanOfficer.zip }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      loanOfficers: [],
+      filterValue: "UT",
+      StateOptions: [],
+    };
+  },
+  methods: {
+    filterLoanOfficer(filterVal) {
+      this.filterValue = filterVal;
+    },
+    makeStateOptionArray() {
+      let arrayTemplate = [];
+      this.loanOfficers.forEach((loanOfficer) => {
+        console.log(loanOfficer);
+        arrayTemplate.push(loanOfficer.state);
+      });
+      this.StateOptions = Array.from(new Set(arrayTemplate));
+      console.log(this.StateOptions);
+    },
+  },
+  async created() {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://api.simplenexus.com/group_profiles/json/c7598ae3-64c4-4e8e-b625-c73e68444640/loanofficers.json",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => (this.loanOfficers = result.loanOfficers))
+      .then((result) => this.makeStateOptionArray(result.loanOfficers))
+      .catch((error) => console.log("error", error));
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<template>
+  <body>
+    <header>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container">
+          <p></p>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-8 mx-auto"></div>
+        </div>
+      </section>
+    </main>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </body>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+</style>
