e3af21a04c832a7fa01a4378ed8240b2fc8ce663
JSlaugh <J@slaugh.net>
2023-10-20 21:31:42 -0600

    comments

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div>
+      <!-- Dynamic Options display -->
       <h2>Options</h2>
       <div
         v-for="(stateOption, key) in StateOptions"
@@ -11,6 +12,7 @@
         {{ stateOption }}
       </div>
     </div>
+    <!-- Dynamic List of loanOfficers -->
     <div
       v-for="(loanOfficer, key) in loanOfficers.filter(
         (item) => item.state == filterValue
@@ -39,14 +41,17 @@ export default {
     };
   },
   methods: {
+    // Function to change the applied filter to displayed loan officers
     filterLoanOfficer(filterVal) {
       this.filterValue = filterVal;
     },
+    // Function to build State Options Array
     makeStateOptionArray() {
       let arrayTemplate = [];
       this.loanOfficers.forEach((loanOfficer) => {
         arrayTemplate.push(loanOfficer.state);
       });
+      // Reduces the array down by removing duplicates
       this.StateOptions = Array.from(new Set(arrayTemplate));
     },
   },
@@ -55,13 +60,15 @@ export default {
       method: "GET",
       redirect: "follow",
     };
-
+    // Fetch Request
     fetch(
       "https://api.simplenexus.com/group_profiles/json/c7598ae3-64c4-4e8e-b625-c73e68444640/loanofficers.json",
       requestOptions
     )
       .then((response) => response.json())
+      // Save data from array
       .then((result) => (this.loanOfficers = result.loanOfficers))
+      // Send the data to our dynamic State Option builder
       .then((result) => this.makeStateOptionArray(result.loanOfficers))
       .catch((error) => console.log("error", error));
   },
